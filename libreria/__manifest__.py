e1ce4c8257bb7b35f75a7f83a6c9402e3acc2546
Juanjo <112622551+baty7@users.noreply.github.com>
2024-02-24 11:45:41 +0100

    Add files via upload

---
+++ libreria/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Gestión de una tienda de de libros    ',
+    'summary':"Gestión de una tienda de de libros ",
+    'description':"""
+    Módulo para la gestión de una tienda de libros
+    """,
+    'author': "Juan José Bautista Gallego",
+
+    'category': 'tools',
+    'version': '0.2',
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        #'security/groups.xml',
+        #'security/ir.model.access.csv',
+        'views/libros.xml',
+        #'views/templates.xml',
+        #'demo/demo.xml',
+    ],
+    
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+    # Pendiente de añadir un report Qweb.
+    
+}
---
+++ libreria/libreria/libros.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+import re
+
+class libros(models.Model):
+    _name = 'libros'
+    _description = 'Modelo de libros donde se almacena informacion sobre cada libro'
+
+    name = fields.Char(string='Nombre',help='NOmbre del libro',required=True)
+    isbn = fields.Char(string="ISBN",required=True)
+    author = fields.Char(string="Autor")
+    desctiption= fields.Text(string="Descripción")
+    price=fields.Float(string="Precio")
+    quantity = fields.Integer(string="Cantidad")
+    
+    @api.model
+    def get_genre(self):
+        genre = [
+            ("fantasia","Fantasía"),
+            ("romance","Romance")
+        ]
+        return genre
+    genre = fields.Selection(get_genre,string="Género")
+
+    @api.model
+    def get_state(self):
+        state = [
+            ("fantasia","Fantasía"),
+            ("romance","Romance"),
+            ("ciencia_ficcion","Ciencia ficción")
+        ]
+        return state
+    state = fields.Selection(get_state,string="Estado")
+
+
+    
+
+
+
+    
+    
+    
+      
+    
+    
