9aeaacefff12ebc7aaf67cbd94e03248c422bab4
Juanjo <juanjosebautistagallego@gmail.com>
2024-02-25 18:51:37 +0100

    mejor interfaz sale_order

--- models/libreria_sale_order.py
+++ models/libreria_sale_order.py
@@ -12,4 +12,7 @@ class LibreriaSaleOrder(models.Model):
     line_ids = fields.One2many('libreria.historico.sale.order', inverse_name ='order_id', string='Historial libros')
     libro_id = fields.Many2one('libros', string='Libro')
     partner_id = fields.Many2one('res.partner',string="Cliente")
+    numero_socio = fields.Char(string='Número de Socio',related='partner_id.numero_socio')
+    telefono = fields.Char(string='Teléfono',related='partner_id.phone')
+    email = fields.Char(string='Correo electrónico',related='partner_id.email')
     
