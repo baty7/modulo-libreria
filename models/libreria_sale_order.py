7d41130527b37f00dc75f178d579ed1ab6b7f485
Juanjo <juanjosebautistagallego@gmail.com>
2024-02-26 19:33:50 +0100

    cambiando vista sale_order de libreria

--- models/libreria_sale_order.py
+++ models/libreria_sale_order.py
@@ -11,7 +11,7 @@ class LibreriaSaleOrder(models.Model):
     is_socio = fields.Boolean(string='Socio')
     line_ids = fields.One2many('libreria.historico.sale.order', inverse_name ='order_id', string='Historial libros')
     libro_id = fields.Many2one('libros', string='Libro')
-    partner_id = fields.Many2one('res.partner',string="Cliente",required=False)
+    #partner_id = fields.Many2one('res.partner',string="Cliente",required=False)
     numero_socio = fields.Char(string='Número de Socio',required=True)
     telefono = fields.Char(string='Teléfono',related='partner_id.phone')
     email = fields.Char(string='Correo electrónico',related='partner_id.email')
