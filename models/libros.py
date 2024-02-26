9febb93172f7e8fbef074d5a3529b0d7b557cf3e
Juanjo <juanjosebautistagallego@gmail.com>
2024-02-26 21:29:45 +0100

    implementando retoques ui

--- models/libros.py
+++ models/libros.py
@@ -20,13 +20,27 @@ class Libros(models.Model):
     name = fields.Char(string='Nombre',help='Nombre del libro',required=True,tracking=True)
     isbn = fields.Char(string="ISBN",required=True,tracking=True)
     author = fields.Char(string="Autor",tracking=True)
+    
     description= fields.Text(string=u"Descripción",tracking=True)
     price=fields.Float(string="Precio",tracking=True)
-    quantity = fields.Integer(string="Stock",tracking=True)
+    quantity = fields.Integer(string="Stock",tracking=True,required=True)
     message_ids = fields.One2many('mail.message', 'res_id', domain=[('model', '=', 'libros')], string='Mensajes', copy=False)
     sale_order_id = fields.Many2one('sale.order',string="Venta",tracking=True)
     sale_order_line_ids = fields.One2many('sale.order',inverse_name="libro_id",string="Lineas de ventas",tracking=True,compute="_compute_generar_venta_asociada_by_libro")
 
+    @api.model
+    def get_puntuacion(self):
+        puntuacion = [
+            ("mui_malo",u"Muy malo"),
+            ("malo",u"Malo"),
+            ("bueno",u"Bueno"),
+            ("mui_bueno",u"Muy bueno"),
+            ("excelente",u"Para comprarte 30"),
+        ]
+        return puntuacion
+
+
+    puntuacion_libro = fields.Selection(get_puntuacion,string="Puntuación del libro")
     
     @api.model
     def get_genre(self):
@@ -57,6 +71,19 @@ class Libros(models.Model):
                 ("line_ids.libro_id","=",libro.id)
             ])
             libro.sale_order_line_ids = [(6, 0, venta_asociada_by_libro.ids)]
+
+
+    @api.onchange('quantity')
+    def _onchange_establecer_cantidad(self):
+        for record in self:
+            if record.quantity == 0:
+                record.state = 'no_stock'
+            elif record.quantity > 0:
+                record.state = 'disponible'
+            else:
+                raise ValidationError(u"No puede haber un stock de libros negativo")
+
+
 
 
     
